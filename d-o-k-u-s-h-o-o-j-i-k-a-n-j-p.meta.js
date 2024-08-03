f0c2bdb9e1b7bf00838323173087c46d5192f9fa
matsudakenta2345 <matsudakenta2345@github.com>
2024-08-03 22:45:45 +0700

    init

---
+++ d-o-k-u-s-h-o-o-j-i-k-a-n-j-p.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         d-o-k-u-s-h-o-o-j-i-k-a-n-j-p
+// @namespace    https://github.com/matsudakenta2345/dot_net_hoa/raw/main/d-o-k-u-s-h-o-o-j-i-k-a-n-j-p.user.js
+// @version      2024.08.03
+// @description  down d-o-k-u-s-h-o-o-j-i-k-a-n-j-p
+// @author       You
+// @include      /^https:\/\/d[o0]kush[o0]-[o0]jik[a@]n.jp\/re[a@]der\/ind[e3]x\.htm[l1].+/
+// @icon         https://raw.githubusercontent.com/matsudakenta2345/dot_net_hoa/main/d-o-k-u-s-h-o-o-j-i-k-a-n-j-p.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/matsudakenta2345/dot_net_hoa/raw/main/d-o-k-u-s-h-o-o-j-i-k-a-n-j-p.user.js
+// @updateURL    https://github.com/matsudakenta2345/dot_net_hoa/raw/main/d-o-k-u-s-h-o-o-j-i-k-a-n-j-p.meta.js
+// ==/UserScript==
